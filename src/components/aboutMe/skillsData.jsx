5975896677cd16f886b1b7d5e3c857d6848c42e4
Ben <cooperben83@gmail.com>
2020-10-17 13:22:29 -0500

    rearranged skills

--- src/components/aboutMe/skillsData.jsx
+++ src/components/aboutMe/skillsData.jsx
@@ -2,9 +2,9 @@ export const skills = [
   { name: "web development", level: 2.5 },
   { name: "product design", level: 3 },
   { name: "user research", level: 2 },
-  { name: "3d modeling", level: 2.5 },
+  { name: "project management", level: 2 },
   { name: "data science", level: 1.5 },
-  { name: "aws cloud", level: 1 },
+  { name: "3d modeling", level: 2 },
   { name: "chemical engineering", level: 2 },
-  { name: "beer brewing", level: 2 },
+  { name: "aws cloud", level: 1 },
 ];
