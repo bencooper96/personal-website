13951f8ad16b968ac1adaf4dbfc95b4ddb22c694
Ben <cooperben83@gmail.com>
2020-12-26 19:25:33 -0600

    format fix for herd logo

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ae93bc36ed2f57122992";
+/******/ 	var hotCurrentHash = "ecb95ef49920e7581d52";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
